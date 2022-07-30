530f5dc4f9a3f1ea4b4192fe23e674b4a73c82b3
Guillermo Verón <guiver95@gmail.com>
2022-07-30 17:44:36 -0400

    v-model and bind

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,30 @@
     v-model:salutation="model.salutation"
     v-model:name="model.name"
   />
+  <BaseInput v-model="model.email" type="email" @blur="onBlurEmail($event)" />
   <pre>{{ model }}</pre>
 </template>
 
 <script>
 import { reactive } from "@vue/reactivity";
 import SalutationName from "./components/SalutationName.vue";
+import BaseInput from "./components/BaseInput.vue";
 
 export default {
   name: "App",
-  components: { SalutationName },
+  components: { SalutationName, BaseInput },
   setup() {
     const model = reactive({
       salutation: "",
       name: "",
+      email: "",
     });
 
-    return { model };
+    function onBlurEmail(event) {
+      window.console.log("The email is: " + event.target.value);
+    }
+
+    return { model, onBlurEmail };
   },
 };
 </script>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input
+      v-bind="$attrs"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    modelValue: {
+      type: [Number, String],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
